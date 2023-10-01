baa87d3c746618dd8dfe0cf4699be06af72f8251
roman <1997pom@gmail.com>
2023-10-01 15:42:53 +0400

    fix deploy vite https

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,7 @@ export default defineConfig({
         }),
     ],
     server: {
-        host: true,
-        strictPort: true,
-        port: 8000,
+        https: true,
+        host: '0.0.0.0'
     },
 });
